79b827ee4d18367346e5f44381058c5f141fb0a1
Rafael Sant'Ana <rafael.santana@dcc.ufmg.br>
2024-08-18 14:25:48 -0300

    feat: alinhar reversi com a classe abstrata

--- include/games/reversi.h
+++ include/games/reversi.h
@@ -10,7 +10,8 @@ class Reversi: public Game {
 private:
   std::vector<Player> players; 
   Player next_move_player;
-
+  int board_size;
+  std::vector<std::string> board;
 public:
   void apply_visual_move(int,int);
   Reversi(Player, Player);
@@ -24,6 +25,14 @@ public:
   char get_next_next_move_player_mark();
   std::vector<std::pair<int,int>> get_valid_moves(int, int);
   bool valid_move(int, int);
+
+
+  void show_board();
+  bool is_board_full();
+  void set_in_board(int, int, char);
+  char get_in_board(int, int);
+  int get_amount_of_char_in_board(char);
+  std::vector<std::pair<int, int>> get_positions_of_char_in_board(char);
 };
 
 #endif
--- src/games/reversi.cpp
+++ src/games/reversi.cpp
@@ -15,6 +15,11 @@ void addOrSubtract(int & j, bool alt_desired_move) {
 Reversi::Reversi(Player p1, Player p2) : Game(), next_move_player(p1) {
   players.push_back(p1);
   players.push_back(p2);
+  board_size=8;
+
+  for (int i = 0; i < board_size; i++) {
+    board.push_back("00000000");
+  }
 
   move_non_check(3,3); // p1
   move_non_check(3,4); // p2
@@ -184,3 +189,76 @@ void Reversi::apply_visual_move(int x, int y) {
   Reversi::move(x,y);
 }
 
+
+
+void Reversi::set_in_board(int x, int y, char c) {
+  if (x < 0 || x > board_size || y < 0 || y > board_size) {
+    // tratar excecao do x e y sairem do board
+  }
+  board[x][y] = c;
+}
+
+bool Reversi::is_board_full() {
+  int default_chars=0;
+  for (int i = 0; i < board_size; i++) {
+    for (int j = 0; j < board_size; j++) {
+      if (board[i][j] == '0') {
+        default_chars++;
+      }
+    }
+  }
+
+  return default_chars==0;
+}
+
+void Reversi::show_board() {
+  std:: cout << "  ";
+  for (int i = 0; i < board_size; i++) {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+  for (int i = 0; i < board_size; i++) {
+    std::cout << i << " ";
+    for (int j = 0; j < board_size; j++) {
+      char val = board[i][j];
+      if (val == '1') val='O';
+      else if (val == '2') val = 'X';
+      else val = ' ';
+      std::cout << val << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+char Reversi::get_in_board(int x, int y) {
+  return board[x][y];
+}
+
+int Reversi::get_amount_of_char_in_board(char c) {
+  int amount = 0;
+  for (int i = 0; i < board_size; i++) {
+    for (int j = 0; j < board_size; j++) {
+      if (board[i][j] == c) {
+        amount++;
+      }
+    }
+  }
+
+  return amount;
+} 
+
+std::vector<std::pair<int, int>> Reversi::get_positions_of_char_in_board(char c) {
+  std::vector<std::pair<int, int>> positions;
+
+  for (int i = 0; i < board_size; i++) {
+    for (int j = 0; j < board_size; j++) {
+      if (board[i][j] == c) {
+        std::pair<int, int> position = {i, j};
+
+        positions.push_back(position);
+      }
+    }
+  }
+
+  return positions;
+}
