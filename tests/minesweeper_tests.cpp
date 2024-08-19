ac51b002e686b001426daecbcea3205a7995740c
Luis Edu <lelimasbrito@gmail.com>
2024-08-19 20:35:11 -0300

    Added minesweeper tests

---
+++ tests/minesweeper_tests.cpp
@@ -0,0 +1,10 @@
+#include "../include/games/minesweeper.h"
+#include "CinRedirector.h"
+#include "CoutRedirector.h"
+#include "doctest.h"
+
+TEST_SUITE("Minesweeper") {
+  TEST_CASE("Opening move") {
+    SUBCASE("First house is a zero") {}
+  }
+}
